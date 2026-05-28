0481d38503b2578e57ca9afb11451f6625512803
kj-podonos <263298736+kj-podonos@users.noreply.github.com>
2026-05-28 05:09:12 +0000

    chore: regenerate SDK from onepin-sdks@8f8aef5ee7d839503940bd5f048cf2ff8db19ad2

--- src/onepin/core/client_wrapper.py
+++ src/onepin/core/client_wrapper.py
@@ -28,6 +28,7 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
+            "User-Agent": "onepin/0.2.1",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
